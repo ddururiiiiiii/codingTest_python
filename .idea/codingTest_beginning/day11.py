88e6deabd451026b533ffeb01355e2ce738051d6
ddururiiiiiii <o244227@daum.net>
2023-03-02 07:40:02 +0900

    프로그래머스 - 코딩테스트 입문 (파이썬) day 10-2

--- .idea/codingTest_beginning/day11.py
+++ .idea/codingTest_beginning/day11.py
@@ -11,3 +11,25 @@ def solution(n):
     while n < math.factorial(k):
         k -= 1
     return k
+
+# 주사위의 갯수
+def solution(box, n):
+    answer = 0
+
+    answer = (box[0] //  n) * (box[1] // n) * (box[2] // n)
+    return answer
+
+# 합성수 찾기
+def solution(n):
+    answer = 0
+    num = []
+
+    count = 0
+    for i in range(2, n+1):
+        for j in range(1, i+1):
+
+            if i % j == 0:
+                num.append(i)
+        if num.count(i) >= 3:
+            answer += 1
+    return answer
