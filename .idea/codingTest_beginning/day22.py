8cfb3c54e0ae5f12d8b712a1e61403adf52c0bec
ddururiiiiiii <o244227@daum.net>
2023-03-17 05:31:41 +0900

    프로그래머스 - 코딩테스트 입문 (파이썬) day 22~23

--- .idea/codingTest_beginning/day22.py
+++ .idea/codingTest_beginning/day22.py
@@ -22,3 +22,16 @@ def solution(n):
             answer += 1
     return answer
 
+# 평행
+def solution(dots):
+    answer = 0
+    if slope(dots[0],dots[1]) == slope(dots[2],dots[3]):
+        answer = 1
+    if slope(dots[0],dots[2]) == slope(dots[1],dots[3]):
+        answer = 1
+    if slope(dots[0],åçdots[3]) == slope(dots[1],dots[2]):
+        answer = 1
+    return answer
+
+def slope(dot1,dot2):
+    return (dot2[1] - dot1[1] ) / (dot2[0] - dot1[0])
