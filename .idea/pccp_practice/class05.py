05f17cf0eeffeb643cecaf1f671a04bb1f4e6e75
ddururiiiiiii <o244227@daum.net>
2023-03-30 08:08:21 +0900

    [PCCP] 파트5.DFS(깊이우선탐색) - 피로도

---
+++ .idea/pccp_practice/class05.py
@@ -0,0 +1,16 @@
+# 피로도
+answer = 0
+def DFS(k, cnt, dungeons, ch):
+    global answer
+    answer = max(answer, cnt)
+    for i in range(len(dungeons)):
+        if k > dungeons[i][0] and ch[i] == 0:
+            ch[i] = 1
+            DFS(k-dungeons[i][1], cnt+1, dungeons, ch)
+            ch[i] = 0
+
+def solution(k, dungeons):
+    ch = [0] * len(dungeons)
+    DFS(k, 0, dungeons, ch)
+
+    return answer
