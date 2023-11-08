7bc7c1891ebd3ce63dc9bfb697bec8f05d739b25
ddururiiiiiii <o244227@daum.net>
2023-11-09 08:39:20 +0900

    프로그래머스 입문 1일차

---
+++ .idea/codingTest_begginng_231109/day01.py
@@ -0,0 +1,31 @@
+# 두수의 합
+def solution1(num1, num2):
+    answer = -1
+
+    answer = num1 + num2
+    print("두수의 합 : ", answer)
+    return answer
+
+    # 두수의 차
+def solution2(num1, num2):
+    answer = -1
+
+    answer = num1 - num2
+    print("두수의 차 : ", answer)
+    return answer
+
+    # 두수의 곱
+def solution3(num1, num2):
+    answer = -1
+
+    answer = num1 * num2
+    print("두수의 곱 : ", answer)
+    return answer
+
+    # 두수의 몫
+def solution4(num1, num2):
+    answer = -1
+
+    answer = num1 // num2
+    print("두수의 몫 : ", answer)
+    return answer
